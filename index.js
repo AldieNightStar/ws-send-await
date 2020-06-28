01d4b15c8daaf1926b34cf42dc3b9d3ee704c4e0
HaxiDenti <aldie.nightstar@gmail.com>
2020-06-28 12:03:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 function addSendAwait(ws) {
-	let cbs = [];
-	ws.on('message', msg => {
-		cbs.forEach(c => c(msg));
-		cbs = [];
-	});
 	ws.sendAwait = function(message) {
 		return new Promise((resolve, reject) => {
-			cbs.push(resolve);
-			ws.send(message);	
+			ws.on('message', function listener(msg) {
+				resolve(msg);
+				ws.removeListener("message", listener);
+			});
+			ws.send(message);
 		});
 	}
 }
